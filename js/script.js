3d7aacbefad9450b9945b921e0245609f29ea8d4
Gianufio83 <fiore.gianuario@gmail.com>
2020-02-03 15:35:53 +0100

    prove Gennaio 2018

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@
 
 $(document).ready(function() {
 
- // var mese = moment([2020, 0, 1]).subtract(24, 'months');
- // console.log(mese);
+ var mese = moment([2020, 0, 1]).subtract(24, 'months');
+ console.log(mese);
  // var data = mese.format('YYYY MM DD') ;
  // console.log(data);
  // var monthsDays = mese.date();
  // console.log(monthsDays);
+ var calendar = {
+   Capodanno : 'Capodanno',
+   Epifania : 'Epifania'
+ };
  $.ajax(
     {
       'url' : 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0.',
@@ -20,6 +24,7 @@ $(document).ready(function() {
       'success': function(data, stato) {
         // console.log(data);
         giorniFestivi(data.response);
+
 
       },
       'error' : function(request, state, errors) {
@@ -33,7 +38,7 @@ $(document).ready(function() {
        var feste = day[i];
        var source = $("#entry-template").html();
        var template = Handlebars.compile(source);
-       var html = template(feste);
+       var html = template(calendar);
        $('.months-container').append(html);
      }
    }
